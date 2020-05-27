ce2d63fac567fbb09f723cc652c70a72e57d4e66
yojik <yoji.kanno@gmail.com>
2020-05-27 07:09:26 +0000

    いったん

--- pages/sample/index.vue
+++ pages/sample/index.vue
@@ -1,38 +1,27 @@
 <template>
   <div>
-    <!--App.vueのtemplate内に記述-->
 
-    <a-scene vr-mode-ui="enterVRButton: #myEnterVRButton; enterARButton: #myEnterARButton">
-      <a id="myEnterVRButton" class="button" href="#">VR</a><!--DOM要素はオーバーレイさせて表示/youtubeもいける-->
-      <a id="myEnterARButton" class="button" href="#">AR</a>
-      <a-assets>
-        <audio id="click-sound" src="audio/click.ogg"></audio>
-        <!-- Images. -->
-        <img id="city" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg" />
-        <img
-          id="city-thumb"
-          src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-city.jpg"
-        />
-        <img id="cubes" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/cubes.jpg" />
-        <img
-          id="cubes-thumb"
-          src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg"
-        />
-        <img id="sechelt" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg" />
-        <img
-          id="sechelt-thumb"
-          src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg"
-        />
-      </a-assets>
-      <!-- 360-degree image. -->
-      <a-sky id="image-360" radius="10" src="#city"></a-sky>
-      <!-- Link we will build. -->
-      <a-entity
-        class="link"
-        geometry="primitive: plane; height: 1; width: 1"
-        material="shader: flat; src: #cubes-thumb"
-        position="0 0 -5"
-      ></a-entity>
+
+    <div class="overlayStyle" id="overlay"> <!--オーバーレイコントロールはフルスクリーンにすると消える問題あり-->
+        <div>
+          <a id="myEnterVRButton"  href="#">VR</a>
+          <a id="myEnterARButton"  href="#">AR</a>
+          <a id="myEnterARButton" href="#">Exit AR</a>
+        </div>
+        <!-- <iframe class="youtube" id="ytplayer" type="text/html" width="240" height="160"
+  src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+  frameborder="0"></iframe> -->
+    </div>
+    <!--App.vueのtemplate内に記述-->
+    <a-scene
+     webxr="optionalFeatures: dom-overlay; overlayElement: #overlay"
+     vr-mode-ui="enterVRButton: #myEnterVRButton; enterARButton: #myEnterARButton; exitARbutton:#myExitARButton">>
+
+      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow></a-box>
+      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow></a-sphere>
+      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" shadow></a-cylinder>
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow></a-plane>
+
       <!-- Camera + Cursor. -->
       <a-camera>
         <a-cursor id="cursor">
@@ -65,9 +54,14 @@ export default {};
 </script>
 
 <style scoped>
-.button {
-  position: absolute;
-  top: 100px;
-  z-index: 2;
+a-scene {
+  height: 300px;
+  width: 600px;
 }
+.overlayStyle {
+  position: absolute;
+  top: 0px;
+  z-index: 1;
+}
+
 </style>
