f96967bdceba70ccebf113f77d2dcc24631a2ba7
YojiKanno <Kanno_Yoji@ogis-ri.co.jp>
2020-05-27 12:24:48 +0900

    プロト作成中・・

--- pages/sample/index.vue
+++ pages/sample/index.vue
@@ -1,41 +1,73 @@
 <template>
   <div>
-<!--App.vueのtemplate内に記述-->
-<a-scene>
-  <a-assets>
-    <audio id="click-sound" src="audio/click.ogg"></audio>
-    <!-- Images. -->
-    <img id="city" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg">
-    <img id="city-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-city.jpg">
-    <img id="cubes" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/cubes.jpg">
-    <img id="cubes-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg">
-    <img id="sechelt" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg">
-    <img id="sechelt-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg">
-  </a-assets>
-  <!-- 360-degree image. -->
-  <a-sky id="image-360" radius="10" src="#city"></a-sky>
-  <!-- Link we will build. -->
-  <a-entity class="link" geometry="primitive: plane; height: 1; width: 1"
-                  material="shader: flat; src: #cubes-thumb"
-                  position="0 0 -5"></a-entity>
-  <!-- Camera + Cursor. -->
-  <a-camera>
-    <a-cursor id="cursor">
-      <a-animation begin="click" easing="ease-in" attribute="scale"
-                   fill="backwards" from="0.1 0.1 0.1" to="1 1 1" dur="150"></a-animation>
-      <a-animation begin="cursor-fusing" easing="ease-in" attribute="scale"
-                   from="1 1 1" to="0.1 0.1 0.1" dur="1500"></a-animation>
-    </a-cursor>
-  </a-camera>
-</a-scene>
+    <!--App.vueのtemplate内に記述-->
+
+    <a-scene vr-mode-ui="enterVRButton: #myEnterVRButton; enterARButton: #myEnterARButton">
+      <a id="myEnterVRButton" class="button" href="#">VR</a><!--DOM要素はオーバーレイさせて表示/youtubeもいける-->
+      <a id="myEnterARButton" class="button" href="#">AR</a>
+      <a-assets>
+        <audio id="click-sound" src="audio/click.ogg"></audio>
+        <!-- Images. -->
+        <img id="city" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg" />
+        <img
+          id="city-thumb"
+          src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-city.jpg"
+        />
+        <img id="cubes" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/cubes.jpg" />
+        <img
+          id="cubes-thumb"
+          src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg"
+        />
+        <img id="sechelt" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg" />
+        <img
+          id="sechelt-thumb"
+          src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg"
+        />
+      </a-assets>
+      <!-- 360-degree image. -->
+      <a-sky id="image-360" radius="10" src="#city"></a-sky>
+      <!-- Link we will build. -->
+      <a-entity
+        class="link"
+        geometry="primitive: plane; height: 1; width: 1"
+        material="shader: flat; src: #cubes-thumb"
+        position="0 0 -5"
+      ></a-entity>
+      <!-- Camera + Cursor. -->
+      <a-camera>
+        <a-cursor id="cursor">
+          <a-animation
+            begin="click"
+            easing="ease-in"
+            attribute="scale"
+            fill="backwards"
+            from="0.1 0.1 0.1"
+            to="1 1 1"
+            dur="150"
+          ></a-animation>
+          <a-animation
+            begin="cursor-fusing"
+            easing="ease-in"
+            attribute="scale"
+            from="1 1 1"
+            to="0.1 0.1 0.1"
+            dur="1500"
+          ></a-animation>
+        </a-cursor>
+      </a-camera>
+    </a-scene>
   </div>
 </template>
 
 <script>
-import 'aframe'
-export default {
-  
-
-
-}
+import "aframe";
+export default {};
 </script>
+
+<style scoped>
+.button {
+  position: absolute;
+  top: 100px;
+  z-index: 2;
+}
+</style>
