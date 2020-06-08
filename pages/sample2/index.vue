cfb5af2230de103abd753af24d3b8fab4478340a
yojik <yoji.kanno@gmail.com>
2020-06-08 14:13:18 +0000

    外部ファイル取り込み

--- pages/sample2/index.vue
+++ pages/sample2/index.vue
@@ -9,12 +9,20 @@
     <a-scene
           webxr="optionalFeatures: dom-overlay; overlayElement: #overlay"
            vr-mode-ui="enterVRButton: #enterVRButton; enterARButton: #enterARButton">
+
+      <a-assets>
+        <a-asset-item id="floor" src="/low_poly_isometric_rooms/scene.gltf"></a-asset-item>
+      </a-assets>
+
+      <a-gltf-model position="0 0 -3.0"  src="#floor"></a-gltf-model>
+
       <a-text position="-1 1.25 -3" value="Hello, World!" color="green"></a-text>
-      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+      <!-- <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
       <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
       <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-      <!-- <a-sky color="#ECECEC"></a-sky> -->
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane> -->
+
+      <a-sky color="#ECECEC"></a-sky>
     </a-scene>
   </div>
 </template>
