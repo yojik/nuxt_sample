a22caddb1cd3f02d94a34dd2f8f994d1c15fa6af
yojik <yoji.kanno@gmail.com>
2020-06-08 03:06:44 +0000

    テキスト追加

--- pages/sample2/index.vue
+++ pages/sample2/index.vue
@@ -9,6 +9,7 @@
     <a-scene
           webxr="optionalFeatures: dom-overlay; overlayElement: #overlay"
            vr-mode-ui="enterVRButton: #enterVRButton; enterARButton: #enterARButton">
+      <a-text position="-1 1.25 -3" value="Hello, World!" color="green"></a-text>
       <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
       <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
       <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
