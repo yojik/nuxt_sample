38b075eeb44a5d79b5544ed21720c971504f4f41
yojik <yoji.kanno@gmail.com>
2020-06-25 09:20:13 +0000

    テンプレートをリテラル化することによってvue.jsとnafの衝突を避けた

--- pages/sample3/index.vue
+++ pages/sample3/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <div class="overlayStyle" id="overlay">
-      <div  >
-        <h2 >networked</h2>
-        <button  id="myEnterVRButton" href="#">VR</button>
+      <div>
+        <h2>networked</h2>
+        <button id="myEnterVRButton" href="#">VR</button>
         <button id="myEnterARButton" href="#">AR</button>
         <button id="myEnterARButton" href="#">Exit AR</button>
       </div>
     </div>
-
     <a-scene
       networked-scene=" app: myApp; room: room1; debug: true;"
       webxr="optionalFeatures: dom-overlay; overlayElement: #overlay"
@@ -20,7 +19,42 @@
           src="/low_poly_isometric_rooms/scene.gltf"
         ></a-asset-item>
 
-        <naf-template id="avatar-template">
+        <naf-template id="avatar-template" v-html="avetarTemplate">
+        </naf-template>
+      </a-assets>
+
+      <a-gltf-model
+        position="0 0 -3.0"
+        scale="2 1 2"
+        src="#floor"
+      ></a-gltf-model>
+      <a-text
+        position="-1 1.25 -3"
+        value="Hello, World!"
+        color="green"
+      ></a-text>
+      <a-entity
+        id="player"
+        position="0 1.3 0 "
+        networked="template:#avatar-template; attachTemplateToLocal:false;"
+        camera
+        wasd-controls
+        look-controls
+      >
+      </a-entity>
+      <a-sky color="#ECECEC"></a-sky>
+    </a-scene>
+  </div>
+</template>
+<script>
+export default {
+  name: "aframe",
+  components: {},
+  mounted() {},
+  computed: {
+    // 算出 getter 関数
+    avetarTemplate() {
+      return `
           <a-entity   class="avatar">
             <a-sphere
               class="head"
@@ -54,37 +88,9 @@
               ></a-sphere
             ></a-entity
             ></a-entity
-          ></naf-template>
-      </a-assets>
-
-      <a-gltf-model
-        position="0 0 -3.0"
-        scale="2 1 2"
-        src="#floor"
-      ></a-gltf-model>
-      <a-text
-        position="-1 1.25 -3"
-        value="Hello, World!"
-        color="green"
-      ></a-text>
-      <a-entity
-        id="player"
-        position="0 1.3 0 "
-        networked="template:#avatar-template; attachTemplateToLocal:false;"
-        camera
-        wasd-controls
-        look-controls
-      >
-      </a-entity>
-      <a-sky color="#ECECEC"></a-sky>
-    </a-scene>
-  </div>
-</template>
-<script>
-export default {
-  name: "aframe",
-  components: {},
-  mounted() {}
+          >  `;
+    }
+  }
 };
 </script>
 
